e6691b2222d0a3059ab061179a9a3aacd7ba0a4c
asypula <a.sypula@stud.elka.pw.edu.pl>
2022-11-16 16:26:43 +0100

    Getting client from database, preparing MySQL database

---
+++ database/schema_16112022.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS parpa;
+
+CREATE TABLE IF NOT EXISTS clients (
+    username varchar(25) NOT NULL,
+    password varchar(255) NOT NULL,
+    mail varchar(255) NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE IF NOT EXISTS cars (
+    registration_nr varchar(7) NOT NULL,
+    username varchar(25) NOT NULL,
+    FOREIGN KEY (username) REFERENCES clients (username)
+);
--- test_db.py
+++ test_db.py
@@ -1,6 +1,17 @@
-from database.db_setup import db_cursor
+from database.db_connector import db_cur, DBConn
+from classes.client import Client
 
-db_cursor.execute("SHOW tables;")
+def prepare_db():
+    """Either create database and tables using DBConn().execute(schema_path)
+        or just insert sample data with insert_path"""
+    schema_path = "database/schema_16112022.sql"
+    insert_path = "database/populate_db_16112022.sql"
+    #DBConn().execute(schema_path)
+    #DBConn().execute(insert_path)
 
-for x in db_cursor:
-    print(x)
+def example(username):
+    my_client=Client.get_client(username)
+    print(f"Username: {my_client.username}, cars: {my_client.cars}")
+
+if __name__=="__main__":
+    example("ola")
