afcce8fb47d093271161fd412dc76e221ddf2676
lidkas <73845996+lidkas@users.noreply.github.com>
2022-11-20 22:32:21 +0100

    PZSP2-14 Changed displaying cars to list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// const CarList = (props) => {
+//   const positions = props.cars.map(function (car) {
+//     <li>{car}</li>
+//   })
+//   return <ol>{positions}</ol>
+// }
+
 function App() {
   const [username, setUsername] = useState(0);
-  const [cars, setCars] = useState(0);
+  const [cars, setCars] = useState([]);
 
   useEffect(() => {
     fetch('/api/example_client').then(res => res.json()).then(data => {
@@ -18,7 +25,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Username: {username}</p>
-        <p>Your cars: {cars}</p>
+        <p>Your cars:</p>
+        <ol>
+          {
+            cars.map(car => {
+              return <li>{car}</li>
+            })
+          }
+        </ol>
       </header>
     </div>
   );
