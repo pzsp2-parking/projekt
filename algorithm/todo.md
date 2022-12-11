9c59474195f44c75fcdc0997d35479d6cd9d94e0
asypula <a.sypula@stud.elka.pw.edu.pl>
2022-12-11 22:26:18 +0100

    Database script added, folder clean-up

--- algorithm/todo.md
+++
@@ -1,1 +0,0 @@
-Folder for algorithm program
--- application/database/populate_db_16112022.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO clients (username, password, mail) VALUES ('ola', 'ola', 'ola');
-INSERT INTO clients (username, password, mail) VALUES ('lidia', 'lidia', 'lidia');
-INSERT INTO clients (username, password, mail) VALUES ('eryk', 'eryk', 'eryk');
-INSERT INTO clients (username, password, mail) VALUES ('arek', 'arek', 'arek');
-
-INSERT INTO cars (registration_nr, username) VALUES ('WZ12345', 'ola');
-INSERT INTO cars (registration_nr, username) VALUES ('WZ23456', 'ola');
-INSERT INTO cars (registration_nr, username) VALUES ('WZ34567', 'lidia');
