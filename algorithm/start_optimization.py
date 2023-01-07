206fa7c3fc616ca7f2a2dd5bb80e3187dce2f4c7
asypula <a.sypula@stud.elka.pw.edu.pl>
2023-01-07 21:11:13 +0100

    Added dashboard in Grafana setup

---
+++ algorithm/start_optimization.py
@@ -0,0 +1,36 @@
+from prometheus_client import start_http_server, Gauge
+from time import sleep
+
+from classes.carpark import Carpark
+from classes.operator_mockup import Operator_mockup
+from classes.balancer import Balancer
+from database.to_database import To_database
+PORT_PROMET=8000
+
+
+
+def main():
+    start_http_server(PORT_PROMET)
+    e_usg = Gauge('energy_usg', 'Energy usage')
+    e_req = Gauge('energy_req', 'Energy requested usage')
+    cars_nr = Gauge('cars_nr', 'Number of cars on the parking')
+    
+    op = Operator_mockup(-50, 150)
+    b = Balancer()
+    cp = Carpark(1)
+
+    while True:
+        demand = op.createDemand()
+        cp.actualize()
+        usage = b.balance(cp, demand)
+        cp.charge()
+        To_database.insert_new_charging(cp.active_charging_stations)
+        To_database.insert_requests(demand, usage, 1)
+        e_usg.set(usage)
+        e_req.set(demand)
+        cars_nr.set(len(cp.active_charging_stations))
+        sleep(5)
+    
+
+if __name__=="__main__":
+    main()
